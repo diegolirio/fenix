9ec4445b5e2332cf3b72d3fcc3e8a17c00715e5c
Diego Lirio <ddamaceno@dellavolpe.com.br>
2017-04-17 14:05:25 -0300

    Implemented Entity Endereco.java

---
+++ src/main/java/com/fenix/entities/Endereco.java
@@ -0,0 +1,110 @@
+package com.fenix.entities;
+
+import org.springframework.data.annotation.Id;
+
+public class Endereco {
+	
+	@Id
+	public Long id;
+
+	public String logradouro;
+	
+	public int numero;
+	
+	public String bairro;
+	
+	public String cidade;
+	
+	public String estado;
+	
+	public String pais;
+	
+	public double latitude;
+	
+	public double longitude;
+
+	public Oficina oficina;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public Oficina getOficina() {
+		return oficina;
+	}
+
+	public void setOficina(Oficina oficina) {
+		this.oficina = oficina;
+	}
+	
+	
+	
+}
